9e241f6a61b05ae80e09bebbd1de2575ed9f47cc
Kolade Olorunnife <kolorunnife@deakin.edu.au>
2019-09-07 11:35:35 +1000

    Added web form route

--- index.py
+++ index.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def root():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/forms')
+def forms():
+    return render_template('web_form.html')
+
+app.run(debug=True)
+    
